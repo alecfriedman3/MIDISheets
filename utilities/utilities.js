bec896a34a279e03cab75be791dfb98a7e9b87d5
Alec Friedman <alec.friedman3@gmail.com>
2016-09-02 11:51:00 -0400

    functioning

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -15,22 +15,16 @@ function getUserMedia(mediaStream){
   analyser.fftSize = 2048;
   analyser.smoothingTimeConstant = 0.85;
   mediaStreamSource.connect( analyser );
-  var frequencyData = new Uint8Array(analyser.fftSize)
+  var frequencyData = new Float32Array(analyser.fftSize)
   var frequencyBlockSize = sampleRate / frequencyData.length
-  // console.log(mediaStreamSource, frequencyData)
 
-  analyser.getByteFrequencyData(frequencyData)
-  var loudest = getPeaks(frequencyData)
-  // var loudest = analyzePeaks(possibleNotes, frequencyData)
-  var loudestInd = frequencyData.indexOf(loudest) + 1
-  // console.log(loudestInd, (frequencyBlockSize * (loudestInd + (loudestInd + 1))) / 2, loudest)
   function that(){
-      analyser.getByteFrequencyData(frequencyData)
-      var loudest = getPeaks(frequencyData)
-      // var loudest = analyzePeaks(possibleNotes, frequencyData)
-      var loudestInd = frequencyData.indexOf(loudest)
-      var freq = loudestInd === -1 ? -1 : (frequencyBlockSize * (loudestInd + (loudestInd + 1))) / 2
-      // console.log(loudestInd, (frequencyBlockSize * (loudestInd + (loudestInd + 1))) / 2, loudest)
+      analyser.getFloatTimeDomainData(frequencyData)
+
+      var loudest = require('detect-pitch')(frequencyData, .25)
+      var freq = !loudest ? -1 : 44100 / loudest
+
+      console.log(freq)
       currentRecording.push(freq)
       interval = window.requestAnimationFrame(that)
   }
@@ -38,38 +32,9 @@ function getUserMedia(mediaStream){
 }
 
 
-function getPeaks(pitchInstance){
-  var peaks = [], startPeak = null, endPeak = null;
-  for (var i = 0; i < pitchInstance.length; i++){
-    if (pitchInstance[i+1] > pitchInstance[i]){
-      startPeak = true;
-    } else if (pitchInstance [i + 1] < pitchInstance[i]){
-      endPeak = true
-    }
-    if (startPeak && endPeak){
-      peaks.push([i, pitchInstance[i]])
-      startPeak = endPeak = null
-    }
-  }
-  return analyzePeaks(peaks, pitchInstance)
-}
-
-function analyzePeaks(possibleNotes, frequencyData){
-  let mean = 0;
-  for (var i = 0; i < frequencyData.length; i++) {
-    var val = frequencyData[i];
-    mean += val*val;
-  }
-  mean = Math.sqrt(mean/frequencyData.length);
-  console.log('mean!', mean,frequencyData)
-  if (mean < 5) return -1; // probably not enough signal
-  return Math.max.apply(null, possibleNotes.map(function (noteArr) { return noteArr[1]}));
-}
-
 module.exports = {
   stopStream: stopStream,
   getUserMedia: getUserMedia,
-  getPeaks: getPeaks,
   getAndClearData: function (){
     var hold = currentRecording;
     currentRecording = []
